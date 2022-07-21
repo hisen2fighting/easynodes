192c5dc3d930e5a7b3c6f7311f104a0c32b65898
ericet <ericetchen@gmail.com>
2022-07-21 05:13:18 +0200

    added tools

---
+++ CosmosTools/autodelegator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source $HOME/.bash_profile
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+WITHOU_COLOR='\033[0m'
+
+
+for (( ;; )); do
+        echo -e "领取奖励。。。"
+        echo -e "${PWDDD}\ny\n" | $PROJECT tx distribution withdraw-rewards ${VAL_ADDR} --chain-id ${CHAIN_ID} --from ${WALLETNAME} --commission --fees ${FEES}${DENOM} -y
+for (( timer=12; timer>0; timer-- ))
+        do
+                printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
+                sleep 1
+        done
+BAL=$($PROJECT q bank balances ${DEL_ADDR} -o json | jq -r '.balances | .[].amount')
+        BAL=$(($BAL- 500))
+echo -e "余额: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${DENOM}\n"
+        echo -e "Stake ALL\n"
+if (( BAL > 500 )); then
+            echo -e "${PWDDD}\n${PWDDD}\n" | $PROJECT tx staking delegate ${VAL_ADDR} ${BAL}${DENOM} --from ${DEL_ADDR} --chain-id ${CHAIN_ID} --fees ${FEES}${DENOM} -y
+        else
+                                echo -e "余额: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} $DENOM BAL < 0 ((((\n"
+        fi
+for (( timer=${DELAY}; timer>0; timer-- ))
+        do
+            printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
+            sleep 1
+        done
+done
---
+++ CosmosTools/tools.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+while true
+do
+
+# Logo
+source ~/.bash_profile
+
+echo "============================================================"
+curl -s https://raw.githubusercontent.com/ericet/easynodes/master/logo.sh | bash
+echo "============================================================"
+
+
+PS3='选择一个操作 '
+options=(
+"设置自动复利" 
+"退出")
+select opt in "${options[@]}"
+               do
+                   case $opt in
+                   
+"设置自动复利")
+S3='Select an action: '
+options=(
+"自动复利设置" 
+"开启自动复利"
+"退出")
+select opt in "${options[@]}"
+               do
+                   case $opt in
+"自动复利设置")
+echo "============================================================"
+echo "项目服务名 比如: seid, teritorid"
+echo "============================================================"
+read PROJECT
+echo export PROJECT=${PROJECT} >> $HOME/.bash_profile
+echo "============================================================"
+echo "设置链ID 比如: atlantic-1, teritori-testnet-v2"
+echo "============================================================"
+read CHAIN_ID
+echo export CHAIN_ID=${CHAIN_ID} >> $HOME/.bash_profile
+echo "============================================================"
+echo "代币的单位 比如: usei, utori"
+echo "============================================================"
+read DENOM
+echo export DENOM=${DENOM} >> $HOME/.bash_profile
+echo "============================================================"
+echo "设置操作费"
+echo "============================================================"
+read FEES
+echo export FEES=${FEES} >> $HOME/.bash_profile
+echo "============================================================"
+echo "钱包名称"
+echo "============================================================"
+read WALLETNAME
+echo export WALLETNAME=${WALLETNAME} >> $HOME/.bash_profile
+echo "============================================================"
+echo "钱包密码"
+echo "============================================================"
+read PWDDD
+echo export PWDDD=${PWDDD} >> $HOME/.bash_profile
+echo "============================================================"
+echo "输入钱包密码"
+echo "============================================================"
+VAL_ADDR=$($PROJECT keys show $WALLETNAME --bech val -a)
+echo export VAL_ADDR=${VAL_ADDR} >> $HOME/.bash_profile
+echo "============================================================"
+echo "输入钱包密码设置质押验证人的地址"
+echo "============================================================"
+DEL_ADDR=$($PROJECT keys show $WALLETNAME --bech acc -a)
+echo export DEL_ADDR=${DEL_ADDR} >> $HOME/.bash_profile
+echo "============================================================"
+echo "设置自动复利间隔时间(秒) 比如： 1800 代表每30分钟自动复利"
+echo "============================================================"
+DELAY=$($PROJECT keys show $WALLETNAME --bech acc -a)
+echo export DELAY=${DELAY} >> $HOME/.bash_profile
+
+source $HOME/.bash_profile
+
+
+mkdir $HOME/autodelegate
+wget -O $HOME/autodelegate/start.sh https://raw.githubusercontent.com/ericet/easynodes/master/CosmosTools/autodelegator.sh
+chmod +x $HOME/autodelegate/start.sh
+break
+;;
+            
+"开启自动复利")
+echo "============================================================"
+echo "自动复利开启。。。"
+echo "============================================================"
+apt install screen
+screen -S AutoDelegate
+
+break
+;;
+
+"退出")
+exit
+;;
+
+*) echo "invalid option $REPLY";;
+esac
+done
+done
+
+break
+;;
+ 
+"退出")
+exit
+;;
+
+*) echo "invalid option $REPLY";;
+esac
+done
+done
